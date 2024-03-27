88aae6d20bb2f9ecb319064b4ed50cef7352efe5
Dione Rodrigues <dione.kacoal@gmail.com>
2024-03-27 20:06:20 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//classe que vai conter a logica dos dados, como os dados serão organizados e estruturados
+
+//classe que cria a visualização do html
+
+import { FavoritesView } from "./favorites.js"
+
+new FavoritesView("#app")
